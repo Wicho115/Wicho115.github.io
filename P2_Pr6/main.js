2a943e74129d13afa41afea82e2de2c73169a2eb
Dante <martin.velazquez.luis.alfonso@gmail.com>
2023-10-01 23:04:01 -0600

    Practica 6

---
+++ P2_Pr6/main.js
@@ -0,0 +1,17 @@
+function obtenerReloj(){
+    let date = new Date();
+    let seconds = date.getSeconds().toString().padStart(2, "0");
+    let minutes = date.getMinutes().toString().padStart(2, "0");
+    let militarHour = date.getHours();
+
+    let isPm = militarHour % 12 > 0;
+    let amOrPm = isPm ? "pm" : "am";
+    let hours = `${isPm ? militarHour % 12 : militarHour}`.padStart(2, "0");
+
+    return `${hours} : ${minutes} : ${seconds} ${amOrPm}`;
+}
+
+//Coloco el intervalo en 1001, para poder ver exactamente cuando ya cambio el segundo
+setInterval(() => {
+    console.log(obtenerReloj());
+},1001)
--- README.md
+++ README.md
@@ -7,5 +7,7 @@
 ## [Parcial 2 - Practica 4/JavaScript File](https://wicho115.github.io/P2_Pr4/main.js)
 ## [Parcial 2 - Practica 5](https://wicho115.github.io/P2_Pr5/Practica5.html)
 ## [Parcial 2 - Practica 5/JavaScript File](https://wicho115.github.io/P2_Pr5/main.js)
+## [Parcial 2 - Practica 6](https://wicho115.github.io/P2_Pr6/index.html)
+## [Parcial 2 - Practica 6/JavaScript File](https://wicho115.github.io/P2_Pr6/main.js)
 
 ## [Sitio - Github Pages](https://wicho115.github.io/)
